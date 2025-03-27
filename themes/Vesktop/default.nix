88c87dcf19a4ef0579feec699e2fba3bbfa08164
Red Penguin <redpenguin777@yahoo.com>
2025-03-27 11:51:20 +0000

    Keep only visual themes for Vesktop, begin work on new theme to undo visual refresh

--- themes/Vesktop/default.nix
+++ themes/Vesktop/default.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }: {
   home.packages = [ pkgs.vesktop ];
 
-  home.file = {
-	".config/vesktop/settings".source = ./settings;
-	".config/vesktop/themes".source = ./themes;
-	".config/vesktop/settings.json".source = ./settings.json;
-  };
+ #  home.file = {
+	# ".config/vesktop/themes".source = ./themes;
+	# ".config/vesktop/settings/quickCss.css".source = ./quickCss.css;
+ #  };
 }
